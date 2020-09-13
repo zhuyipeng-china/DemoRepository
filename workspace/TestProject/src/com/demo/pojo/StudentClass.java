fdfc7408b71a786c1881f4d05d59ca8d7686b37a
zhuyipeng <976268399@qq.com>
2020-09-13 17:46:49 +0800

    第一次提交测试

---
+++ workspace/TestProject/src/com/demo/pojo/StudentClass.java
@@ -0,0 +1,22 @@
+package com.demo.pojo;
+
+public class StudentClass {
+	
+	private String class_name;
+	private String class_code;
+	public String getClass_name() {
+		return class_name;
+	}
+	public void setClass_name(String class_name) {
+		this.class_name = class_name;
+	}
+	public String getClass_code() {
+		return class_code;
+	}
+	public void setClass_code(String class_code) {
+		this.class_code = class_code;
+	}
+	
+	
+
+}
