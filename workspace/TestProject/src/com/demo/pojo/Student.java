97c88463e3dcb71499aafbcfea510f781316e50a
zhuyipeng <976268399@qq.com>
2020-09-13 17:29:00 +0800

    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'

---
+++ workspace/TestProject/src/com/demo/pojo/Student.java
@@ -0,0 +1,36 @@
+package com.demo.pojo;
+
+public class Student {
+	
+	private String name;
+	private int age;
+	private String grade;
+	private String sclass;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getSclass() {
+		return sclass;
+	}
+	public void setSclass(String sclass) {
+		this.sclass = sclass;
+	}
+	
+	
+
+}
